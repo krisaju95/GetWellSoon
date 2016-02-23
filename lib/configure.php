92f5cb596cdf3792427e4c93259791d9dc7fc361
root <maheshuligade@gmail.com>
2016-02-23 20:05:32 +0530

    Security Feature Added

--- lib/configure.php
+++ lib/configure.php
@@ -1,7 +1,7 @@
 <?php
         $servername = 'localhost';
-        $username = 'admin';
-        $password = "password";
+        $username = 'root';
+        $password = "mysql";
         $dbname = "health_centre";
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
