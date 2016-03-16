009fc72e01010577d2bb68a86f9c7e008bda7041
healthcentrenitc <healthcentrenitc@gmail.com>
2016-03-16 17:12:58 +0530

    Wed Mar 16 17:12:58 IST 2016

--- autoupload.sh
+++ autoupload.sh
@@ -6,8 +6,8 @@ if [[ `git status --porcelain` ]]; then
    #echo "$(git commit -m (echo $(echo $(date))))";
    #echo "$(git commit -m $(echo \" $(echo $(date)$(echo \"))))"
   echo "$(git commit -m "`date`")"; 
-  echo "$(git push auto_upload origin)";
+  echo "$(git push auto_upload master)";
 else
     echo "Unchanged";
-     echo "$(git push auto_upload origin)";
+     echo "$(git push auto_upload master)";
 fi
