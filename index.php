e796222fac97bc2d1d6b527ab41ce78ded644e97
Piyush Bhopalka <pbhopalka@gmail.com>
2016-08-11 23:54:41 +0530

    Update login interface completely

--- index.php
+++ index.php
@@ -1,106 +1,112 @@
 <!--
- * File name: index.php
- * The first page in the website. The login page.
+* File name: index.php
+* The first page in the website. The login page.
 -->
 
 <?php
 
+error_reporting(E_ERROR);
 include("lib/configure.php");
 session_start();
 if(isset($_SESSION['login_type'])){
-	if($_SESSION['login_type']=="admin")
-	{
-		header("location: home.php");
-	}
-	else if($_SESSION['login_type']=="Doctor")
-	{
-		header("location: doctor_home.php");
-	}
-	else if($_SESSION['login_type']=="lab_admin")
-	{
-		header("location: lab_admin_home.php");
-	}
+    if($_SESSION['login_type']=="admin")
+    {
+        header("location: home.php");
+    }
+    else if($_SESSION['login_type']=="Doctor")
+    {
+        header("location: doctor_home.php");
+    }
+    else if($_SESSION['login_type']=="lab_admin")
+    {
+        header("location: lab_admin_home.php");
+    }
 }
 
 $error="";
 
 if(isset($_POST['submit'])) {
-	session_start();
-
-	// Define $myusername and $mypassword
-	$myusername=$_POST['username'];
-	$mypassword=$_POST['password'];
-
-
-	$myusername = stripslashes($myusername);
-	$mypassword = stripslashes($mypassword);
-	$myusername = mysqli_real_escape_string($conn,$myusername);
-	$mypassword = sha1(mysqli_real_escape_string($conn,$mypassword));
-
-	$sql="SELECT * FROM users WHERE UserName='$myusername' and Password='$mypassword'";
-	$result=mysqli_query($conn, $sql);
-
-// Mysql_num_row is counting table row
-	$count=mysqli_num_rows($result);
-
-// If result matched $myusername and $mypassword, table row must be 1 row
-
-	if($count==1)
-	{
-		$row = mysqli_fetch_array($result);
-		$_SESSION['login_user']=$myusername;
-		$_SESSION['login_type']=$row['Type'];
-		if($_SESSION['login_type']=="admin")
-		{
-			header("location: home.php");
-		}
-		else if($_SESSION['login_type']=="Doctor")
-		{
-			header("location: doctor_home.php");
-		}
-		else
-		{
-			header("location: lab_admin_home.php");
-		}
-	}
-	else {
-		$error.= "<br>Invalid username or password.";
-	}
+    session_start();
+    
+    // Define $myusername and $mypassword
+    $myusername=$_POST['username'];
+    $mypassword=$_POST['password'];
+    
+    
+    $myusername = stripslashes($myusername);
+    $mypassword = stripslashes($mypassword);
+    $myusername = mysqli_real_escape_string($conn,$myusername);
+    $mypassword = sha1(mysqli_real_escape_string($conn,$mypassword));
+    
+    $sql="SELECT * FROM users WHERE UserName='$myusername' and Password='$mypassword'";
+    $result=mysqli_query($conn, $sql);
+    
+    // Mysql_num_row is counting table row
+    $count=mysqli_num_rows($result);
+    
+    // If result matched $myusername and $mypassword, table row must be 1 row
+    
+    if($count==1)
+    {
+        $row = mysqli_fetch_array($result);
+        $_SESSION['login_user']=$myusername;
+        $_SESSION['login_type']=$row['Type'];
+        if($_SESSION['login_type']=="admin")
+        {
+            header("location: home.php");
+        }
+        else if($_SESSION['login_type']=="Doctor")
+        {
+            header("location: doctor_home.php");
+        }
+        else
+        {
+            header("location: lab_admin_home.php");
+        }
+    }
+    else {
+        $error = "Invalid Username or Password.";
+    }
 }
 
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="icon" href="images/cross.png" type="image/gif" sizes="16x16">
-<title>NITC Health Centre</title>
-<link href="css/login.css" type="text/css" rel="stylesheet"/>
-<style type="text/css">
-body,td,th {
-	font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", Verdana, sans-serif;
-}
-</style>
-</head>
+  <!doctype html>
+  <html>
 
-<body bgcolor="1b1b1b">
-<div class=logo></div>
-<div class="login-form">
-	<h1>Login </h1>
-	<form action="" method="post">
-		<li>
-			<input type="text" name='username' class="text" autocomplete="on" placeholder="User Name"><p class=" icon user"></p>
-		</li>
-		<li>
-			<input name = 'password' type="password" placeholder="Password"><p class=" icon lock"></p>
-		</li>
-        <div>
-			<input type="submit" name="submit" value="Sign In" >
-		</div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="images/cross.png" type="image/x-icon" property="og:image" />
+    <title>NITC Health Centre</title>
+    <!--<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+    <link href="css/materialize.min.css" type="text/css" rel="stylesheet" />
+    <link href="css/login.css" type="text/css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <div class="card">
+      <div class="top">
+        <h3>NITC Health Centre </h3>
         <span class="form_error"><?php echo $error; ?></span>
-	</form>
-	<a href="features/forgot_password.php" id="forgot_pw">Forgot Password</a>
-</div>
-</body>
-</html>
+        <div class="content">
+          <form action="" method="post">
+            <div class="input-container">
+              <li>
+                <input type="text" name='username' class="text" autocomplete="on" placeholder="User Name" />
+              </li>
+              <li>
+                <input name='password' type="password" placeholder="Password" />
+              </li>
+              <li>
+                <a href="features/forgot_password.php" id="forgot_pw">Forgot Password</a>
+              </li>
+            </div>
+            <input class="bottom-content" type="submit" name="submit" value="Sign In" />
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </body>
+
+  </html>
